a4fd4a6d673d1d66cd1858ca67f6abebcaa6ca33
unknown <filip.trifunovic23@gmail.com>
2023-03-08 14:44:59 +0100

    creating header component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Header from "@/components/Header";
 
 export const metadata = {
   title: "IMDB Clone",
@@ -8,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Header />
       <body>{children}</body>
     </html>
   );
