822912f5a44853c54ea8a4d72c22b663ad054096
unknown <filip.trifunovic23@gmail.com>
2023-03-08 14:00:55 +0100

    installing tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -4,5 +4,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Hello</h1>;
+  return <h1 className="text-red-500">HOME</h1>;
 }
